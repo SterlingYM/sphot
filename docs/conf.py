bbdef1acf59a90453c32ce5275cfdd64065a7d7b
SterlingYM <sterling.astro@berkeley.edu>
2024-07-23 23:22:10 -0400

    bug fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,3 +28,8 @@ language = 'python'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+import os
+import sys
+#Location of Sphinx files
+sys.path.insert(0, os.path.abspath('../'))
